c0ed689c5b0274fbfb8f86f4f3e850b67d8ae27a
Enzo Pelizaro <160739594+egplzr@users.noreply.github.com>
2025-04-07 20:13:10 -0300

    Add files via upload

---
+++ DesenvolvimentoSistemasWeb/tads-dswb-7abr25/exemplo-rest/src/main/java/br/senac/tads/dsw/exemplo/rest/DadosDto.java
@@ -0,0 +1,133 @@
+package br.senac.tads.dsw.exemplo.rest;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
+
+public class DadosDto {
+  private Integer id;
+
+  //@NotNull
+  //@NotEmpty
+  @NotBlank
+  private String nome;
+
+  private String apelido;
+
+  @PastOrPresent
+  private LocalDate dataNascimento;
+
+  @NotBlank
+  @Email
+  private String email;
+
+  private String telefone;
+
+  private String senha;
+
+  private String senhaConfirmacao;
+
+  private List<String> interesses;
+
+  public DadosDto() {
+  }
+
+  public DadosDto(Integer id, String nome, String dataNascimento,
+      String email, String telefone) {
+    this.id = id;
+    this.nome = nome;
+    this.dataNascimento = LocalDate.parse(dataNascimento);
+    this.email = email;
+    this.telefone = telefone;
+  }
+
+  public DadosDto(Integer id, String apelido, String nome, String dataNascimento,
+      String email, String telefone, List<String> interesses) {
+    this.id = id;
+    this.apelido = apelido;
+    this.nome = nome;
+    this.dataNascimento = LocalDate.parse(dataNascimento);
+    this.email = email;
+    this.telefone = telefone;
+    this.interesses = interesses;
+
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getApelido() {
+    return apelido;
+  }
+
+  public void setApelido(String apelido) {
+    this.apelido = apelido;
+  }
+
+  public LocalDate getDataNascimento() {
+    return dataNascimento;
+  }
+
+  public void setDataNascimento(LocalDate dataNascimento) {
+    this.dataNascimento = dataNascimento;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getTelefone() {
+    return telefone;
+  }
+
+  public void setTelefone(String telefone) {
+    this.telefone = telefone;
+  }
+
+  public String getSenha() {
+    return senha;
+  }
+
+  public void setSenha(String senha) {
+    this.senha = senha;
+  }
+
+  public String getSenhaConfirmacao() {
+    return senhaConfirmacao;
+  }
+
+  public void setSenhaConfirmacao(String senhaConfirmacao) {
+    this.senhaConfirmacao = senhaConfirmacao;
+  }
+
+  public List<String> getInteresses() {
+    return interesses;
+  }
+
+  public void setInteresses(List<String> interesses) {
+    this.interesses = interesses;
+  }
+
+  
+
+}
